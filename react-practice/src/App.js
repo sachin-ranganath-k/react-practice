95ca60bfd506c075942cf17121555ab44216c3f7
sachin-ranganath-k <ranganathkulkarni20@gmail.com>
2022-11-09 12:08:34 +0530

    static design like udemy cards using props

--- react-practice/src/App.js
+++ react-practice/src/App.js
@@ -1,42 +1,44 @@
-import Picture from "./images/appu.jfif";
+import Card from "./Card";
+import Picture from "./images/udemy.jpg";
+// import './App.css'
 
 const App = () => {
   return (
-    <div className="ui comments">
+    <div className="ui container">
       <div className="ui four column grid">
         <div className="row">
-          <div className="column">
-            <div className="comment">
-              <a href="/" className="avatar">
-                <img alt="Profile Picture" src={Picture} />
-              </a>
-              <div className="content">
-                <a href="/" className="author">
-                  Ranganath K
-                </a>
-                <div className="metadata">
-                  <span className="date">Today 08-11-2022 !</span>
-                </div>
-                <div className="text">ReactJS Bolg</div>
-              </div>
-            </div>
-          </div>
-          <div className="column">
-            <div className="comment">
-              <a href="/" className="avatar">
-                <img alt="Profile Picture" src={Picture} />
-              </a>
-              <div className="content">
-                <a href="/" className="author">
-                  Puneeth R K
-                </a>
-                <div className="metadata">
-                  <span className="date">Yesterday 07-11-2022</span>
-                </div>
-                <div className="text">NodeJS Bolg</div>
-              </div>
-            </div>
-          </div>
+          <Card
+            authorName="Ranganath K"
+            noOfRatings="3599"
+            title="ReactJS Guide"
+            picture={Picture}
+            price="&#x20b9;3000"
+          />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Card
+            authorName="Puneeth R K"
+            noOfRatings="35959"
+            title="NodeJS Guide"
+            picture={Picture}
+            price="&#x20b9;2500"
+          />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Card
+            authorName="Roberrt"
+            noOfRatings="35499"
+            title="Javascript Guide"
+            picture={Picture}
+            price="&#x20b9;3300"
+          />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Card
+            authorName="William Stallings"
+            noOfRatings="36599"
+            title="Web Guide"
+            picture={Picture}
+            price="&#x20b9;3900"
+          />
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         </div>
       </div>
     </div>
---
+++ react-practice/src/Card.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Rating } from "semantic-ui-react";
+// import "./Card.css";
+
+const Card = (propsFromApp) => {
+  console.log(propsFromApp);
+
+  return (
+    <div>
+      <div className="column">
+        <div className="ui centered card" id="cards" style={{ width: "auto" }}>
+          <div className="image">
+            <img src={propsFromApp.picture} />
+          </div>
+          <div className="content" style={{ lineHeight: "1.5" }}>
+            <a className="header" style={{ lineHeight: "1.5" }}>
+              {propsFromApp.title}
+            </a>
+            <div className="meta">
+              <span className="date">{propsFromApp.authorName}</span>
+            </div>
+            <Rating maxRating={5} defaultRating={3} icon="star" size="mini" />(
+            {propsFromApp.noOfRatings})
+            <div className="description">
+              <h4> {propsFromApp.price}</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
